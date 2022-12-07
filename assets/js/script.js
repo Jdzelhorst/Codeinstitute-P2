fc7074ce130441823482683bfbdcdcdf20e5af13
Jdzelhorst <jandouwezelhorst@gmail.com>
2022-12-07 15:36:53 +0000

    Add function to show selected font button + Add RNG for computerchoice and show generatod computerchoice font

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,36 @@ for (let button of choiceButtons) {
     });
 }
 
+// Adding function to select the right Font with the right gamerChoice and computerChoice
+// Adding the randomnumber generator for the computerChoice
+
+function playGame(gamerChoice) {
+    if (gamerChoice == "0") {
+        gamerChoiceFont.className = "fa-solid fa-hand-back-fist";
+    } else if (gamerChoice == "1"){
+        gamerChoiceFont.className = "fa-solid fa-hand";
+    } else if (gamerChoice == "2"){
+        gamerChoiceFont.className = "fa-solid fa-scissors";
+    } else if (gamerChoice == "3"){
+        gamerChoiceFont.className = "fa-solid fa-hand-lizard";
+    } else {
+        gamerChoiceFont.className = "fa-solid fa-hand-spock";
+    }
+
+    computerChoice = Math.floor(Math.random() * 5);
+
+    if (computerChoice == "0") {
+        computerChoiceFont.className = "fa-solid fa-hand-back-fist";
+    } else if (computerChoice == "1"){
+        computerChoiceFont.className = "fa-solid fa-hand";
+    } else if (computerChoice == "2"){
+        computerChoiceFont.className = "fa-solid fa-scissors";
+    } else if (computerChoice == "3"){
+        computerChoiceFont.className = "fa-solid fa-hand-lizard";
+    } else {
+        computerChoiceFont.className = "fa-solid fa-hand-spock";
+    }
+
+    checkWinner();
+
+}
